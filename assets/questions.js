08cf68632d43dbba0635c71747463fa8ce4749cf
austin green <trentgr33n@icloud.com>
2022-05-14 12:51:14 -0400

    rough draft commit

---
+++ assets/questions.js
@@ -0,0 +1,55 @@
+let questions = [
+    {
+    numb: 1,
+    question: "What does .js stand for?",
+    answer: "JavaScript",
+    options: [
+      "JavaScript",
+      "Job Security",
+      "Java School",
+      "Java Speed"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What HTML element do we put in the JavaScript?",
+    answer: "<script>",
+    options: [
+      "<scripting>",
+      "<javascript>",
+      "<js>",
+      "<script>"
+    ]
+  },
+    {
+    numb: 3,
+    question: "JavaScript is the same as java.",
+    answer: "false",
+    options: [
+      "true",
+      "false"
+     
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which event occurs when a user clicks on an HTML element?",
+    answer: "onclick",
+    options: [
+      "onchange",
+      "onclick",
+      "onmouseover",
+      "onmouseclick"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How do you declare a JavaScript variable?",
+    answer: "var carName;",
+    options: [
+      "v carName",
+      "variable carName;",
+      "var carName;"
+    
+    ]
+  },];
